07d1eacac43cbc1b64f42d3845926c3bdce5dc1d
Ruhtra0116 <150013559+Ruhtra0116@users.noreply.github.com>
2024-09-15 16:53:41 +0800

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -124,7 +124,7 @@ def main():
         """
         <style>
         .main {
-            background-image: url('https://www.istockphoto.com/illustrations/music-background');
+            background-image: url('https://media.istockphoto.com/id/1076840920/vector/music-background.jpg?s=612x612&w=0&k=20&c=bMG2SEUYaurIHAjtRbw7bmjLsXyT7iJUvAM5HjL3G3I=');
             background-size: cover;
             background-position: center;
         }
